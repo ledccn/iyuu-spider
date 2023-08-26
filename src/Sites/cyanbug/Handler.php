dd879e3964e2abc56c02de9b6b66b55025f12657
david <367013672@qq.com>
2023-08-27 00:22:29 +0800

    新增：大青虫

---
+++ src/Sites/cyanbug/Handler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Iyuu\Spider\Sites\cyanbug;
+
+use Iyuu\Spider\Frameworks\NexusPHP\Parser;
+
+/**
+ * 爬虫句柄
+ * - dirname:cyanbug
+ */
+class Handler extends Parser
+{
+    const SITE_NAME = 'cyanbug';
+}
