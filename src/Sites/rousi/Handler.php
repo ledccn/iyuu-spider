4ebcc6e0944fe6f43825db679e684a082709c170
david <nz1985@qq.com>
2023-08-26 13:17:52 +0800

    新增站点：肉丝

---
+++ src/Sites/rousi/Handler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Iyuu\Spider\Sites\rousi;
+
+use Iyuu\Spider\Frameworks\NexusPHP\Parser;
+
+/**
+ * 爬虫句柄
+ * - dirname:rousi
+ */
+class Handler extends Parser
+{
+    const SITE_NAME = 'rousi';
+}
