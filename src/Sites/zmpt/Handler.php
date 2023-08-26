4650fee93289ae9645809b24d3d0e690934e40e3
david <nz1985@qq.com>
2023-08-26 12:10:50 +0800

    新增：织梦PT

---
+++ src/Sites/zmpt/Handler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Iyuu\Spider\Sites\zmpt;
+
+use Iyuu\Spider\Frameworks\NexusPHP\Parser;
+
+/**
+ * 爬虫句柄
+ * - dirname:zmpt
+ */
+class Handler extends Parser
+{
+    const SITE_NAME = 'zmpt';
+}
