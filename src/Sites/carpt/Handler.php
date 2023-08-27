c29e45af56b79a03ce96480d68bbec1be5c56b5c
david <nz1985@qq.com>
2023-08-27 17:48:32 +0800

    新增：车站

---
+++ src/Sites/carpt/Handler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Iyuu\Spider\Sites\carpt;
+
+use Iyuu\Spider\Frameworks\NexusPHP\Parser;
+
+/**
+ * 爬虫句柄
+ * - dirname:carpt
+ */
+class Handler extends Parser
+{
+    const SITE_NAME = 'carpt';
+}
