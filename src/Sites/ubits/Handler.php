35fa5e9320e8fc28cc02e8cf625fdcf1ae1f1689
david <nz1985@qq.com>
2023-08-27 12:26:33 +0800

    新增站点：ubits

---
+++ src/Sites/ubits/Handler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Iyuu\Spider\Sites\ubits;
+
+use Iyuu\Spider\Frameworks\NexusPHP\Parser;
+
+/**
+ * 爬虫句柄
+ * - dirname:ubits
+ */
+class Handler extends Parser
+{
+    const SITE_NAME = 'ubits';
+}
