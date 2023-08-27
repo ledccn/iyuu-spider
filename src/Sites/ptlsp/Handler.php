424e302053947d03d481f2e56115e20f7c225990
david <nz1985@qq.com>
2023-08-27 12:02:26 +0800

    新增站点：ptslp

---
+++ src/Sites/ptlsp/Handler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Iyuu\Spider\Sites\ptlsp;
+
+use Iyuu\Spider\Frameworks\NexusPHP\Parser;
+
+/**
+ * 爬虫句柄
+ * - dirname:ptlsp
+ */
+class Handler extends Parser
+{
+    const SITE_NAME = 'ptlsp';
+}
