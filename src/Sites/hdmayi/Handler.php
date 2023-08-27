47cb7b93f70d49376c1078be451424d003ecbdee
david <nz1985@qq.com>
2023-08-27 17:33:45 +0800

    新增：蚂蚁/熊猫

---
+++ src/Sites/hdmayi/Handler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Iyuu\Spider\Sites\hdmayi;
+
+use Iyuu\Spider\Frameworks\NexusPHP\Parser;
+
+/**
+ * 爬虫句柄
+ * - dirname:hdmayi
+ */
+class Handler extends Parser
+{
+    const SITE_NAME = 'hdmayi';
+}
---
+++ src/Sites/pandapt/Handler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Iyuu\Spider\Sites\pandapt;
+
+use Iyuu\Spider\Frameworks\NexusPHP\Parser;
+
+/**
+ * 爬虫句柄
+ * - dirname:pandapt
+ */
+class Handler extends Parser
+{
+    const SITE_NAME = 'pandapt';
+}
