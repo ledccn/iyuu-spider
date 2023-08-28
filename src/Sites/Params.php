29ea53288ea316a07a8d56fc0004d35e30fe6f2e
david <nz1985@qq.com>
2023-08-28 08:21:14 +0800

    fix

--- src/Sites/Params.php
+++ src/Sites/Params.php
@@ -6,13 +6,13 @@ use Iyuu\Spider\Support\DataStruct;
 
 /**
  * 爬取参数
- * @property string site 站点名称
- * @property string action 动作
- * @property string type 爬虫类型:cookie,rss
- * @property string uri 统一资源标识符
- * @property string begin 开始页码
- * @property string end 结束页码
- * @property bool daemon 守护进程
+ * @property string $site 站点名称
+ * @property string $action 动作
+ * @property string $type 爬虫类型:cookie,rss
+ * @property string $uri 统一资源标识符
+ * @property string $begin 开始页码
+ * @property string $end 结束页码
+ * @property bool $daemon 守护进程
  */
 class Params extends DataStruct
 {
