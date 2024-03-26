7c4404ed9f9dc012c1214256b21004990f8e67dc
david <367013672@qq.com>
2024-03-26 13:19:36 +0800

    fix

--- src/Container.php
+++ src/Container.php
@@ -2,6 +2,8 @@
 
 namespace Iyuu\Spider;
 
+use Closure;
+
 /**
  * 容器
  */
@@ -9,7 +11,7 @@ class Container extends \think\Container implements \Ledc\Pipeline\Container
 {
     /**
      * 容器对象实例
-     * @var \think\Container|Closure
+     * @var Container|\Ledc\Pipeline\Container|Closure
      */
     protected static $instance;
 }
